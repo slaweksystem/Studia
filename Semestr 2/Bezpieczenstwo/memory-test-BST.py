688c29d17e6909356209a7973f45f821ec31227d
slaweksystem <slaweksystem@gmail.com>
2025-06-14 01:46:57 +0200

    pomiary poprawione

---
+++ Semestr 2/Bezpieczenstwo/memory-test-BST.py
@@ -0,0 +1,13 @@
+# importy lokalnych modułów
+from BST import HashTable as HashTableBST
+
+from Benchmark import memory_benchmark
+from Generators import generate_keys
+
+if __name__ == '__main__':
+    test_size = 1000000
+    keys = generate_keys(test_size, 'Losowa')
+    memory_used = memory_benchmark(HashTableBST, keys)
+
+    print(f"BST Ilosc elementów: {test_size}: Zużyta Pamiec: {memory_used} MiB")
+
